94a3e862dfe2b5e6b844c1a541fb5ef69ec54972
andrebowen <95552325+andrebowen@users.noreply.github.com>
2026-08-14 00:24:42 +1000

    Kill the iOS picker tap flash, and give Terms and Privacy real page margins.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SlabSet v23 - bump VERSION on deploy so caches refresh.
-var VERSION = 'v23-20260814-4';
+var VERSION = 'v23-20260814-5';
 var CACHE = 'slabset-' + VERSION;
 var ASSETS = [
   './',
